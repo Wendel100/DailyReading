92125f3d4d2cfed9887aa154350ec3ffc00fae5b
Wendel <renanwendel100@gmail.com>
2026-02-22 20:10:19 -0300

    version 1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Bootstrap funcionando 🚀</h1>
+      <button className="btn btn-success">Clique aqui</button>
+    </div>
+    
+  );
+}
+
+export default App
